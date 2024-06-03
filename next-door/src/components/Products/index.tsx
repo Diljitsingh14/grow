3c82c2da767d304060c18a42f443e2c117f56c92
Diljitsingh14 <jeetcreation14@gmail.com>
2024-06-02 23:58:20 -0400

    init: product FE

---
+++ next-door/src/components/Products/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ProductList = [
+  {
+    google_review_nfc_qr: {
+      name: "Google Review NFC + QR",
+      key: "grNQ",
+      label: "Google Review NFC with QR",
+    },
+    google_revie_qr: {
+      name: "Google Review NFC + QR",
+      key: "grQ",
+      label: "Google Review NFC with QR",
+    },
+  },
+];
+
+const Product = () => {
+  return <div className="product"></div>;
+};
+
+const Products = () => {
+  return <></>;
+};
+
+export default Products;
