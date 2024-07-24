fc2bdcaaf5af9fc11b349c1681d8230e422a46da
Diljitsingh14 <jeetcreation14@gmail.com>
2024-07-24 01:08:08 -0400

    FE/feat: login form

---
+++ tap-wizard/src/pages/my_account/login/index.tsx
@@ -0,0 +1,169 @@
+import React, { useState, ChangeEvent, FormEvent } from "react";
+import axios from "axios";
+import { HOST, LOGIN } from "@/constants/urls";
+
+interface LoginData {
+  username: string;
+  password: string;
+}
+
+const Login: React.FC = () => {
+  const [loginData, setLoginData] = useState<LoginData>({
+    username: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleDataChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setLoginData({ ...loginData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: FormEvent): Promise<void> => {
+    e.preventDefault();
+    setError(null);
+    setLoading(true);
+
+    if (loginData.password && loginData.username) {
+      const url = `${HOST}/${LOGIN}`;
+      console.log("click", url, HOST);
+      try {
+        const res = await axios.post(url, loginData);
+        console.log(res.data);
+      } catch (err) {
+        console.error(err);
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      setLoading(false);
+      setError("Please fill in both fields.");
+    }
+  };
+
+  return (
+    <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div className="relative bg-blue-500 bg-opacity-50 rounded-lg shadow-lg p-8">
+          <div className="absolute inset-0 bg-white bg-opacity-30 backdrop-blur-sm filter blur-lg rounded-lg"></div>
+          <div className="relative z-10">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <img
+                className="mx-auto h-10 w-auto"
+                src="/images/logo/brand-dark.png"
+                alt="Your Company"
+              />
+              <hr className="mt-4" />
+              {/* <h2 className="mt-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Login
+              </h2> */}
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              {error && (
+                <div className="text-red-500 text-center mb-4">{error}</div>
+              )}
+              <form className="space-y-6" onSubmit={handleSubmit}>
+                <div>
+                  <label
+                    htmlFor="username"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    User name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="username"
+                      name="username"
+                      type="text"
+                      required
+                      value={loginData.username}
+                      onChange={handleDataChange}
+                      className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Password
+                    </label>
+                    <div className="text-sm">
+                      <a
+                        href="#"
+                        className="font-semibold text-indigo-600 hover:text-indigo-500"
+                      >
+                        Forgot password?
+                      </a>
+                    </div>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      required
+                      value={loginData.password}
+                      onChange={handleDataChange}
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    disabled={loading}
+                  >
+                    {loading ? (
+                      <svg
+                        className="animate-spin h-5 w-5 text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <circle
+                          className="opacity-25"
+                          cx="12"
+                          cy="12"
+                          r="10"
+                          stroke="currentColor"
+                          strokeWidth="4"
+                        ></circle>
+                        <path
+                          className="opacity-75"
+                          fill="currentColor"
+                          d="M4 12a8 8 0 018-8v8H4z"
+                        ></path>
+                      </svg>
+                    ) : (
+                      "Sign in"
+                    )}
+                  </button>
+                </div>
+              </form>
+
+              <p className="mt-10 text-center text-sm text-gray-500">
+                Not a member?{" "}
+                <a
+                  href="#"
+                  className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                >
+                  Start a 14 day free trial
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
